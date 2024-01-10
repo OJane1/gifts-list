f55bddbfa8c09f00b12d8b675341342fc9d70b1c
OJane1 <o.s.petrashova@gmail.com>
2024-01-10 10:08:05 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { data } from './data';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [gifts, setGifts] = useState(data);
+
+const removeGift = (id) => {
+  let newGifts = gifts.filter(gift => gift.id !== id);
+  setGifts(newGifts);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="container">
+      <h1>List of {gifts.length} gifts</h1>
+    </div>
+    <div className='container'>
+    {gifts.map((element => {
+      const { id, gift, image } = element;
+
+      return(
+        <div>
+          <div className='container'>
+            <h2>{id} - {gift}</h2>
+          </div>
+          <div className='container'>
+            <img src={image} width="300px" />
+          </div>
+          <div className='container'>
+            <button onClick={()=> removeGift(id)} className='btn'>Remove</button>
+          </div>
+        </div>
+      )
+    }))}
+    <div className='container'>
+      <button onClick={() => setGifts([])} className='btn'>DELETE ALL</button>
+    </div>
+    </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+        id: 1,
+        gift: "iPhone 15 Pro",
+        image: "https://images.unsplash.com/photo-1695048065127-fbee942eed2e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aXBob25lJTIwMTV8ZW58MHx8MHx8fDA%3D"
+    },
+
+    {
+        id: 2,
+        gift: "SPA treat",
+        image: "https://images.unsplash.com/photo-1544161515-4ab6ce6db874?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3BhfGVufDB8fDB8fHww"
+    },
+
+    {
+        id: 3,
+        gift: "Trip to Japan",
+        image: "https://images.unsplash.com/photo-1526481280693-3bfa7568e0f3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGphcGFufGVufDB8fDB8fHww"
+    },
+
+    {
+        id: 4,
+        gift: "Sports outfit",
+        image: "https://images.unsplash.com/photo-1575052814086-f385e2e2ad1b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZpdG5lc3N8ZW58MHx8MHx8fDA%3D"
+    },
+
+    {
+        id: 5,
+        gift: "Shoes",
+        image: "https://images.unsplash.com/photo-1519415943484-9fa1873496d4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGlnaCUyMGhlZWxlZCUyMHNob2VzfGVufDB8fDB8fHww"
+    }
+]
